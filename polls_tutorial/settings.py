16cf1db2dcfc99cfa98a69e6010db33ab1a60586
nareynolds <nathaniel.reynolds@gmail.com>
2015-06-08 18:03:48 -0400

    set to use PostgreSQL

--- polls_tutorial/settings.py
+++ polls_tutorial/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'polls_tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'pollster',
+        'PASSWORD': 'anotherbigstrongpassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
